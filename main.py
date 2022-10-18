1526824bda4b14094ff3c3be74f6c3b0bf9768d2
Geovany94 <chespy.chompira@mail.ru>
2022-10-18 17:31:01 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Ejercicio 1
+
+print("Por Favor ingrese numeros enteros.")
+a= int(input('Ingrese el primer Numero: '))
+b = int(input('Ingrese el segundo numero: '))
+c = int(input('Ingrese el tercer numero: '))
+d = int(input('Ingrese el cuarto Numero: '))
+
+lista = [a, b, c, d ]
+def orden(e):
+    e.sort() # Para ordenar numeros de forma descendente y si  sn letras , alfabeticamente
+    e.reverse()# Para capturar el srot() ordenado, y reversed lo invierte
+    for h in e:
+        print(h)
+
+orden(lista)
+#hola
+#efe
